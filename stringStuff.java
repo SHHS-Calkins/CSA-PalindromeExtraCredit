13d71e7d144c61cd77aff0531ae21224d8db6451
SHHS-Calkins <12359618@c-vusd.org>
2025-11-07 18:04:32 +0000

    v2

---
+++ stringStuff.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import stuff.methodClass;
+
+public class stringStuff {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String word1 = "";
+        String word2 = "";
+        int choice = 0;
+        System.out.print("Welcome! ");
+        while (choice != 4) {
+        System.out.println("Please choose your action(1-3):");
+        System.out.println("1. Get the reverse of a word.");
+        System.out.println("2. Check if two words are the same.");
+        System.out.println("3. Check if a word is a Palindrome.");
+        System.out.println("4. Quit the program.");
+            choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    scan.nextLine();
+                    word1 = scan.nextLine();
+                    System.out.println(methodClass.reverseWord(word1));
+                    break;
+                case 2:
+                    System.out.println("1st Word:");
+                    scan.nextLine();
+                    word1 = scan.nextLine();
+                    System.out.println("2nd Word:");
+                    word2 = scan.nextLine();
+                    System.out.println("1st Word = 2nd Word?: " + word1.equalsIgnoreCase(word2));
+                    break;
+                case 3:
+                    scan.nextLine();
+                    System.out.println("Word: ");
+                    word1 = scan.nextLine();
+                    System.out.println(methodClass.isPalindrome(word1));
+                    break;
+                case 4:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("I said 1-3.");
+                    break;
+            }
+        }
+        scan.close();
+    }
+}
--- stuff/methodClass.java
+++ stuff/methodClass.java
@@ -1,12 +1,13 @@
-public class reverse {
-    public String reverseWord(String word) {
+package stuff;
+public class methodClass {
+    public static String reverseWord(String word) {
         String result = "";
         for(int i = word.length(); i > 0; i--) {
             result += word.substring(i-1, i);
         }
         return result;
     }
-    public boolean checkPalindrome(String word) {
+    public static boolean isPalindrome(String word) {
         return reverseWord(word).equals(word);
     }
 }
